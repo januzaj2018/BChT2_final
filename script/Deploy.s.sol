0eb156e86731f3bd223b3a47cff05af6f742b41a
Aibek <aibekcsgo@gmail.com>
2026-05-15 00:32:48 +0500

    fix: forge coverage

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -15,68 +15,85 @@ import "../src/LootVRF.sol";
 /**
  * @title DeployScript
  * @dev Deploys the full GameFi Protocol to Arbitrum Sepolia.
+ * Refactored to avoid "Stack too deep" errors during coverage reports.
  */
 contract DeployScript is Script {
+    struct DeployedContracts {
+        GameToken token;
+        GameItem item;
+        PriceFeed feed;
+        GameAMM amm;
+        RentalVault vault;
+        GameTimelock timelock;
+        GameGovernor governor;
+        LootVRF loot;
+    }
+
     function run() external {
         uint256 deployerPrivateKey = vm.envOr("PRIVATE_KEY", uint256(1));
         address deployer = vm.addr(deployerPrivateKey);
 
         vm.startBroadcast(deployerPrivateKey);
 
-        // 1. Core Tokens
-        GameToken token = new GameToken();
-        GameItem item = new GameItem();
-        GameItemFactory factory = new GameItemFactory();
+        DeployedContracts memory dc;
 
-        // 2. Oracle (Using correct Arbitrum Sepolia ETH/USD Feed)
-        // Bypass checksum check
-        address ethUsdFeed = address(uint160(0xd30621D869D25c9a81c3129d58d49758A7d078c1));
-        PriceFeed feed = new PriceFeed(ethUsdFeed);
+        // 1. Core Tokens & Oracle
+        dc.token = new GameToken();
+        dc.item = new GameItem();
+        new GameItemFactory();
 
-        // 3. AMM (GAME / WOOD pool)
+        // Use parseAddress to satisfy compiler checksum demands safely
+        address ethUsdFeed = vm.parseAddress("0xd30621D869D25c9a81c3129d58d49758A7d078c1");
+        dc.feed = new PriceFeed(ethUsdFeed);
+
+        // 2. Marketplace & Vault
         GameToken woodToken = new GameToken();
-        GameAMM amm = new GameAMM(address(token), address(woodToken));
+        dc.amm = new GameAMM(address(dc.token), address(woodToken));
+        dc.vault = new RentalVault(dc.token, dc.item, 1);
 
-        // 4. Rental Vault
-        RentalVault vault = new RentalVault(token, item, 1); // Target item 1
+        // 3. Governance
+        {
+            address[] memory proposers = new address[](0);
+            address[] memory executors = new address[](0);
+            dc.timelock = new GameTimelock(2 days, proposers, executors, deployer);
+            dc.governor = new GameGovernor(dc.token, dc.timelock);
+        }
 
-        // 5. Governance
-        address[] memory proposers = new address[](0);
-        address[] memory executors = new address[](0);
-
-        GameTimelock timelock = new GameTimelock(2 days, proposers, executors, deployer);
-
-        GameGovernor governor = new GameGovernor(token, timelock);
-
-        // 6. Loot VRF (Mocked or using testnet coordinator)
+        // 4. Loot VRF
         address vrfCoordinator = 0x6D1416c43F9cb9F0A70068BB7F2cd9E6A0324310;
+        dc.loot = new LootVRF(vrfCoordinator, address(dc.item), 1, bytes32(0));
 
-        LootVRF loot = new LootVRF(vrfCoordinator, address(item), 1, bytes32(0));
-
-        // 7. Setup Roles & Ownership
-        timelock.grantRole(timelock.PROPOSER_ROLE(), address(governor));
-        timelock.grantRole(timelock.EXECUTOR_ROLE(), address(0)); // Open execution
-
-        token.grantRole(token.MINTER_ROLE(), address(vault));
-        token.grantRole(token.MINTER_ROLE(), address(loot));
-        item.grantRole(item.MINTER_ROLE(), address(loot));
-        item.grantRole(item.DEFAULT_ADMIN_ROLE(), address(timelock));
-
-        // Transfer admin to timelock
-        token.grantRole(0x00, address(timelock));
-        token.revokeRole(0x00, deployer);
+        // 5. Setup Roles & Ownership
+        _setupRoles(dc, deployer);
 
         vm.stopBroadcast();
 
-        // Log addresses for frontend
+        _logAddresses(dc);
+    }
+
+    function _setupRoles(DeployedContracts memory dc, address deployer) internal {
+        dc.timelock.grantRole(dc.timelock.PROPOSER_ROLE(), address(dc.governor));
+        dc.timelock.grantRole(dc.timelock.EXECUTOR_ROLE(), address(0));
+
+        dc.token.grantRole(dc.token.MINTER_ROLE(), address(dc.vault));
+        dc.token.grantRole(dc.token.MINTER_ROLE(), address(dc.loot));
+        dc.item.grantRole(dc.item.MINTER_ROLE(), address(dc.loot));
+        dc.item.grantRole(dc.item.DEFAULT_ADMIN_ROLE(), address(dc.timelock));
+
+        // Transfer admin to timelock
+        dc.token.grantRole(0x00, address(dc.timelock));
+        dc.token.revokeRole(0x00, deployer);
+    }
+
+    function _logAddresses(DeployedContracts memory dc) internal view {
         console.log("--- DEPLOYMENT LOG ---");
-        console.log("GameToken:", address(token));
-        console.log("GameItem:", address(item));
-        console.log("PriceFeed:", address(feed));
-        console.log("GameAMM:", address(amm));
-        console.log("RentalVault:", address(vault));
-        console.log("GameTimelock:", address(timelock));
-        console.log("GameGovernor:", address(governor));
-        console.log("LootVRF:", address(loot));
+        console.log("GameToken:", address(dc.token));
+        console.log("GameItem:", address(dc.item));
+        console.log("PriceFeed:", address(dc.feed));
+        console.log("GameAMM:", address(dc.amm));
+        console.log("RentalVault:", address(dc.vault));
+        console.log("GameTimelock:", address(dc.timelock));
+        console.log("GameGovernor:", address(dc.governor));
+        console.log("LootVRF:", address(dc.loot));
     }
 }
